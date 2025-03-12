6af72295fe53f1369e46e206a05108138e3df167
Edinaldo Bispo <edbispo.dev@gmail.com>
2025-03-12 14:06:28 -0300

    modulo banco criado e conta finalizado

---
+++ banco.py
@@ -0,0 +1,21 @@
+from conta import Conta
+
+class Banco:
+    def __init__(self,nome):
+        self.nome = nome
+        self.contas = {}
+        
+    def criar_conta(self, numero, titular):
+        if numero not in self.contas:
+            self.contas[numero] = Conta(numero, titular)
+            print(f'Conta {numero} criada para {titular}.')
+        else: 
+            print(f'Número da conta já existe')        
+            
+    def get_conta(self, numero):
+         return self.contas.get(numero)
+     
+    def listar_contas(self):
+         print(f'Contas no banco {self.nome}:')
+         for numero, conta in self.contas.items():
+             print(f'Conta {numero} - Titular: {conta.titular} - Saldo: R${conta.saldo} ')       
